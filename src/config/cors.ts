448f401bb3ee2ee52237ec5b7fb9918a59876f16
Santosh Bhulun <santosh@swivt.com.np>
2023-02-08 15:27:56 +0545

    cors updated

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,8 @@
 const Cors = {
   allowedOrigins:
     process.env.ALLOWED_ORIGINS ||
-    'http://localhost:3000,http://localhost:3001,http://localhost:4000,http://localhost:8000',
+    'http://localhost:3000,http://localhost:3001,http://localhost:4000,http://localhost:8000,' +
+      'https://incandescent-toffee-fc14e5.netlify.app/',
   originRegex:
     process.env.ORIGIN_REGEX || 'https?://([a-z0-9]+[.])*domainname[.]com',
 };
